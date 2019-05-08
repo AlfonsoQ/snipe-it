dc91d4381eb411240391ced6da1eb905201410b1
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:49:35 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/companies/general.php
+++ resources/lang/es-MX/admin/companies/general.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'about_companies_title'            => 'Sobre las empresas',
-    'about_companies_text'                  => 'Empresas pueden ser utilizadas como un campo de identificador simple, o pueden utilizarse para limitar la visibilidad de los activos, usuarios, etc, si el soporte completo a la empresa está habilitado en la configuración del Admin.',
+    'about_companies_title'            => 'Sobre las compañias',
+    'about_companies_text'                  => 'Las compañias pueden ser utilizadas como un campo de identificador simple, o pueden utilizarse para limitar la visibilidad de los activos, usuarios, etc, si el modo full company support a está habilitado en la configuración del Admin.',
     'select_company' => 'Seleccionar compañía',
 ];
