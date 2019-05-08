53909a97bce4b6dea33e683e876796d06b45f24c
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:55:02 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/locations/table.php
+++ resources/lang/es-MX/admin/locations/table.php
@@ -3,19 +3,19 @@
 return array(
     'about_locations_title'     => 'Acerca de ubicaciones',
     'about_locations'           => 'Las Ubicaciones son utilizadas para hacer seguimiento de la información sobre ubicación de usuarios, bienes, y otros items',
-    'assets_rtd'                => 'Equipos', // This has NEVER meant Assets Retired. I don't know how it keeps getting reverted.
-    'assets_checkedout'         => 'Equipo asignado',
+    'assets_rtd'                => 'Activos', // This has NEVER meant Assets Retired. I don't know how it keeps getting reverted.
+    'assets_checkedout'         => 'Activos asignados',
     'id'                        => 'ID',
     'city'                      => 'Ciudad',
     'state'                     => 'Provincia',
     'country'                   => 'Pais',
-    'create'                    => 'Crear Localización',
-    'update'                    => 'Actualizar Localización',
-    'name'                      => 'Nombre Localización',
+    'create'                    => 'Crear Ubicación',
+    'update'                    => 'Actualizar Ubicación',
+    'name'                      => 'Nombre Ubicación',
     'address'                   => 'Dirección',
-    'zip'                       => 'Códio Postal',
-    'locations'                 => 'Localizaciones',
-    'parent'                    => 'Padre',
-    'currency'                  => 'Divisa de la Localización',
+    'zip'                       => 'Código Postal',
+    'locations'                 => 'Ubicaciones',
+    'parent'                    => 'Ubicación padre',
+    'currency'                  => 'Moneda de la Ubicación',
     'ldap_ou'                   => 'Búsqueda LDAP OU',
     );
