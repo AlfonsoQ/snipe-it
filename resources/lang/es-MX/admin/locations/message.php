57434d287c48513edb759db90789e78e2df6a4de
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:53:16 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/locations/message.php
+++ resources/lang/es-MX/admin/locations/message.php
@@ -2,26 +2,26 @@
 
 return array(
 
-    'does_not_exist' => 'Localización no existente.',
-    'assoc_users'	 => 'Esta localización está asignada al menos a un usuario y no puede ser eliminada. ',
-    'assoc_assets'	 => 'Esta ubicacion se encuentra actualmente asociada con por lo menos un activo y no puede ser eliminada. Por favor, actualice sus activos para no referenciar esta ubicacion e intentelo de nuevo. ',
+    'does_not_exist' => 'Ubicación no existente.',
+    'assoc_users'	 => 'Esta ubicación está asignada al menos a un usuario y no puede ser eliminada. ',
+    'assoc_assets'	 => 'Esta ubicación se encuentra actualmente asociada con por lo menos un activo y no puede ser eliminada. Por favor, actualice sus activos para no referenciar esta ubicacion e intentelo de nuevo. ',
     'assoc_child_loc'	 => 'Esta ubicacion actualmente esta asociada con al menos una ubicacion hija y no puede ser eliminada. Por favor, actualice sus ubicaciones para no referenciar esta ubicacion e intentelo de nuevo. ',
 
 
     'create' => array(
-        'error'   => 'Localización no creada, Intentalo de nuevo.',
-        'success' => 'Localización creada.'
+        'error'   => 'Ubicación no creada, Intentalo de nuevo.',
+        'success' => 'Ubicación creada.'
     ),
 
     'update' => array(
-        'error'   => 'Localización no actualizada, Intentalo de nuevo',
-        'success' => 'Localización actualizada.'
+        'error'   => 'Ubicación no actualizada, Intentalo de nuevo',
+        'success' => 'Ubicación actualizada.'
     ),
 
     'delete' => array(
         'confirm'   	=> '¿Está seguro que desea eliminar esta ubicación?',
-        'error'   => 'Localización no eliminada por un problema. Intentalo de nuevo.',
-        'success' => 'Localización eliminada.'
+        'error'   => 'Ubicación no eliminada por un problema. Intentalo de nuevo.',
+        'success' => 'Ubicación eliminada.'
     )
 
 );
