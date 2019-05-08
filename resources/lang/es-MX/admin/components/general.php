68c3f69f4be212be31725da0b8618c3acccf5c40
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:50:30 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/components/general.php
+++ resources/lang/es-MX/admin/components/general.php
@@ -2,10 +2,10 @@
 
 return array(
     'about_components_title' 			=> 'Sobre los componentes',
-    'about_components_text'  			=> 'Un Componente es parte de un Equipo, por ejemplo, Disco Duro, Ram, DVD, etc.',
+    'about_components_text'  			=> 'Un Componente es parte de un Activo, por ejemplo, Disco Duro, Ram, DVD, etc.',
     'component_name'                  => 'Nombre de Componente',
-    'checkin'                             => 'Checkin Componente',
-    'checkout'                             => 'Checkout Componente',
+    'checkin'                             => 'Devolver Componente',
+    'checkout'                             => 'Asignar Componente',
     'cost'				=> 'Costo de compra',
     'create'                             => 'Crear componente',
     'edit'                             => 'Editar componente',
