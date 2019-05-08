35289646eee5a6df829fd0328489d605f8c7a972
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 17:52:07 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/hardware/form.php
+++ resources/lang/es-MX/admin/hardware/form.php
@@ -1,18 +1,18 @@
 <?php
 
 return array(
-	'bulk_delete'		=> 'Confirmar eliminación masiva de bienes',
-  'bulk_delete_help'	=> 'Revisar los bienes para eliminación masiva. Una vez eliminados, esos bienes pueden ser restaurados, pero no volverán a estar asociados con ningún usuario a los que esté asignado.',
+	'bulk_delete'		=> 'Confirmar eliminación masiva de equipos',
+  'bulk_delete_help'	=> 'Revisar los equipos para eliminación masiva. Una vez eliminados, esos equipos pueden ser restaurados, pero no volverán a estar asociados con ningún usuario a los que esté asignado.',
   'bulk_delete_warn'	=> 'Está a punto de borrar :asset_count.',
-	'bulk_update'		=> 'Actualización masiva de Equipos',
-	'bulk_update_help'	=> 'Este formulario le permite actualizar múltiples Equipos simultáneamente. Solo complete los campos que quiere modificar. Todo campo en blanco permanecerá sin cambios. ',
+	'bulk_update'		=> 'Actualización masiva de equipos',
+	'bulk_update_help'	=> 'Este formulario le permite actualizar múltiples equipos simultáneamente. Solo complete los campos que quiere modificar. Todo campo en blanco permanecerá sin cambios. ',
 	'bulk_update_warn'	=> 'Esta a punto de editar las propiedades de :asset_count equipos.',
     'checkedout_to'		=> 'Asignado a',
     'checkout_date'		=> 'Fecha de asignación',
     'checkin_date'		=> 'Fecha de devolución',
     'checkout_to'		=> 'Asignar a',
     'cost'				=> 'Precio Compra',
-    'create'			=> 'Crear Equipo',
+    'create'			=> 'Crear equipo',
     'date'				=> 'Fecha Compra',
     'depreciation'	    => 'Depreciación',
     'depreciates_on'	=> 'Se deprecia en',
@@ -36,9 +36,9 @@ return array(
     'serial'			=> 'N. Serie',
     'status'			=> 'Estado',
     'tag'				=> 'Etiqueta',
-    'update'			=> 'Actualizar Equipo',
+    'update'			=> 'Actualizar equipo',
     'warranty'			=> 'Garantía',
-		'warranty_expires'		=> 'Vencimiento de la Garantía',
+		'warranty_expires'		=> 'Vencimiento de la garantía',
     'years'				=> 'años',
 )
 ;
