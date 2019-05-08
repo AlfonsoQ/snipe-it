6b651f3bcdb21695807301a83e8f2d10d74f84f2
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:51:50 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/components/message.php
+++ resources/lang/es-MX/admin/components/message.php
@@ -5,30 +5,30 @@ return array(
     'does_not_exist' => 'El componente no existe.',
 
     'create' => array(
-        'error'   => 'El componente no fuè creado, intentalo de nuevo.',
-        'success' => 'El componente se creò satisfactoriamente.'
+        'error'   => 'El componente no fué creado, intentalo de nuevo.',
+        'success' => 'El componente se creó satisfactoriamente.'
     ),
 
     'update' => array(
-        'error'   => 'El componente no se actualizò, intentalo de nuevo',
+        'error'   => 'El componente no se actualizó, intentalo de nuevo',
         'success' => 'Componente actualizado satisfactoriamente.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Estàs seguro de que quieres borrar este componente?',
+        'confirm'   => 'Estás seguro de que quieres borrar este componente?',
         'error'   => 'Hubo un problema al querer borrar el componente, intentalo de nuevo.',
         'success' => 'El componente fue borrado satisfactoriamente.'
     ),
 
      'checkout' => array(
-        'error'   		=> 'El componente no hizo check out, intentalo de nuevo',
-        'success' 		=> 'Check out del componente satisfactorio.',
+        'error'   		=> 'El componente no fue asignado, intentalo de nuevo',
+        'success' 		=> 'Asignación del componente exitósa.',
         'user_does_not_exist' => 'El usuario es invalido, intentalo de nuevo.'
     ),
 
     'checkin' => array(
-        'error'   		=> 'El componente no fue checked in, intentalo de nuevo',
-        'success' 		=> 'Check in de componente satisfactorio.',
+        'error'   		=> 'El componente no fue devuelto, intentalo de nuevo',
+        'success' 		=> 'Devolución de componente exitósa.',
         'user_does_not_exist' => 'Es usuario es invalido, intentalo de nuevo.'
     )
 
