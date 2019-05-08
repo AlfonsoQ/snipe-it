5048be674ff8f98358afb884d4fa43a6f4b5531c
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:26:05 -0500

    Es-MX Language update

--- resources/lang/es-MX/passwords.php
+++ resources/lang/es-MX/passwords.php
@@ -2,6 +2,6 @@
 
 return [
     'sent'	        => 'El link de la contraseña ha sido enviada!',
-    'user'			=> 'No matching active user found with that email.',
+    'user'			=> 'No encontré un usuario activo con ese correo.',
 ];
 
