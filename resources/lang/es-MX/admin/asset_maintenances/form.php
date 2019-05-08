43568df7a74db6e6163acc7ec3c2fc847affe640
AlfonsoQ <37342954+AlfonsoQ@users.noreply.github.com>
2019-05-08 18:44:13 -0500

    Es-MX Language update

--- resources/lang/es-MX/admin/asset_maintenances/form.php
+++ resources/lang/es-MX/admin/asset_maintenances/form.php
@@ -1,14 +1,14 @@
 <?php
 
     return [
-        'asset_maintenance_type' => 'Tipo de Mantenimiento de Equipo',
+        'asset_maintenance_type' => 'Tipo de Mantenimiento de activo',
         'title'                  => 'Título',
         'start_date'             => 'Fecha de Inicio',
         'completion_date'        => 'Fecha de Terminación',
         'cost'                   => 'Costo',
         'is_warranty'            => 'Mejora de la Garantía',
-        'asset_maintenance_time' => 'Tiempo de Mantenimiento de Equipo (en días)',
+        'asset_maintenance_time' => 'Tiempo de Mantenimiento de activo (en días)',
         'notes'                  => 'Notas',
-        'update'                 => 'Actualizar Mantenimiento de Equipo',
-        'create'                 => 'Crear Mantenimiento de Equipo'
+        'update'                 => 'Actualizar Mantenimiento de activo',
+        'create'                 => 'Crear Mantenimiento de activo'
     ];
